83ebe80ffdb1d3f3a715396ec56057a6f2dec4ef
sigidhanafi <sigidhanafi@gmail.com>
2018-03-25 11:33:24 +0700

    add save the game

---
+++ RatingApps/GamePickerViewController.swift
@@ -0,0 +1,76 @@
+//
+//  GamePickerViewController.swift
+//  RatingApps
+//
+//  Created by Sigit Hanafi on 3/25/18.
+//  Copyright © 2018 Sigit Hanafi. All rights reserved.
+//
+
+import UIKit
+
+class GamePickerViewController: UITableViewController {
+    
+    var games = [
+        "Angry Birds",
+        "Chess",
+        "Russian Roulette",
+        "Spin the Bottle",
+        "Texas Hold'em Poker",
+        "Tic-Tac-Toe"
+    ]
+    
+    var selectedGame: String? {
+        didSet {
+            if let selectedGame = selectedGame,
+                let index = games.index(of: selectedGame) {
+                selectedGameIndex = index
+            }
+        }
+    }
+    
+    var selectedGameIndex: Int?
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return games.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "GameCell", for: indexPath)
+        
+        cell.textLabel?.text = games[indexPath.row]
+        
+        if indexPath.row == selectedGameIndex {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+    
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        guard segue.identifier == "SaveSelectedGame",
+            let cell = sender as? UITableViewCell,
+            let indexPath = tableView.indexPath(for: cell) else {
+                return
+        }
+        
+        let index = indexPath.row
+        selectedGame = games[index]
+    }
+}
+
+extension GamePickerViewController {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let index = selectedGameIndex {
+            let cell = tableView.cellForRow(at: IndexPath(row: index, section: 0))
+            cell?.accessoryType = .none
+        }
+        
+        selectedGame = games[indexPath.row]
+        
+        let cell = tableView.cellForRow(at: indexPath)
+        cell?.accessoryType = .checkmark
+    }
+}
--- RatingApps/PlayerDetailsViewController.swift
+++ RatingApps/PlayerDetailsViewController.swift
@@ -15,10 +15,20 @@ class PlayerDetailsViewController: UITableViewController {
     
     var player: Player?
     
+    var game: String = "Chess" {
+        didSet {
+            detailLabel.text = game
+        }
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "SavePlayerDetail",
             let playerName = nameTextField.text {
-            player = Player(name: playerName, game: "Chess", rating: 1)
+            player = Player(name: playerName, game: game, rating: 1)
+        }
+        if segue.identifier == "PickGame",
+            let gamePickerViewController = segue.destination as? GamePickerViewController {
+            gamePickerViewController.selectedGame = game
         }
         
     }
@@ -32,6 +42,13 @@ extension PlayerDetailsViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.section == 0 {
             nameTextField.becomeFirstResponder()
+        }
+    }
+    
+    @IBAction func unwindWithSelectedGame(segue: UIStoryboardSegue) {
+        if let gamePickerViewController = segue.source as? GamePickerViewController,
+            let selectedGame = gamePickerViewController.selectedGame {
+            game = selectedGame
         }
     }
 }
